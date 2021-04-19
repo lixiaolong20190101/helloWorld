0294cf8554df67e0af0292464328340630146eaa
lixiaolong20190101 <45529308+lixiaolong20190101@users.noreply.github.com>
2021-04-19 09:15:25 +0800

    Update Ssmdemo002Application.java
    加了打印语句

--- src/main/java/com/yidu/ssmdemo002/Ssmdemo002Application.java
+++ src/main/java/com/yidu/ssmdemo002/Ssmdemo002Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Ssmdemo002Application {
     public static void main(String[] args) {
+         System.out.println("helloWorld!");
         SpringApplication.run(Ssmdemo002Application.class, args);
     }
 
